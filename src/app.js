3e7bc319fa966edc1c0a88a4c96c35cc314e5124
Michael Aprile Mancha <130480625+mikeaprilee@users.noreply.github.com>
2023-10-22 19:14:32 +0000

    Funciona

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  //Selector de Clases
+  document.querySelector(".Card").classList.add(generateRandomSuit());
+  document.querySelector(".numb").innerHTML = generateRandomNumber();
+  document.querySelector(".button").addEventListener("click", () => {
+    document
+      .querySelector(".Card")
+      .classList.replace(generateRandomSuit(), generateRandomSuit2());
+    document.querySelector(".numb").innerHTML = generateRandomNumber();
+  });
+};
+// Generador de Numberos
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+// Generador de Palos
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "club", "heart"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+let generateRandomSuit2 = () => {
+  let suit2 = ["diamond", "spade", "club", "heart"];
+  let indexSuit2 = Math.floor(Math.random() * suit2.length);
+  return suit2[indexSuit2];
 };
